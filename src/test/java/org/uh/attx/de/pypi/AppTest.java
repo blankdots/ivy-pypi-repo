b9fd6b1f4ba7ee8a46a5ac52b002d0d464d6217e
Stefan Negru <stefan.negru@helsinki.fi>
2017-01-13 08:41:33 +0200

    adding test placeholder

---
+++ src/test/java/org/uh/attx/de/pypi/AppTest.java
@@ -0,0 +1,23 @@
+package org.uh.attx.de.pypi;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by stefanne
+ */
+public class AppTest {
+
+    @Test
+    public void testRunningPort() {
+
+    }
+
+    @Test
+    public void testAddEndpoint() {
+
+    }
+
+}
